25132a349faa8f14f9ac2771cea75becfecc6162
josem <josem@LAPTOP-6NEF8T4E>
2020-04-17 12:22:36 +0200

    insertando objetos

--- src/main/java/rg/quintana/agendajugadoresfutbol/AgendaJugadores.java
+++ src/main/java/rg/quintana/agendajugadoresfutbol/AgendaJugadores.java
@@ -1,10 +1,36 @@
 
 package rg.quintana.agendajugadoresfutbol;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class AgendaJugadores {
 
-    public static void main(String[] args) {
+ public static void main(String[] args) {
+        // Conectar con la base de datos
 
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AgendaJugadoresFutbolPU");
+        EntityManager em = emf.createEntityManager();
+
+        Jugadores JugadoresBarcelona = new Jugadores (0, "Lionel", "Messi");
+
+        Jugadores JugadoresMadrid = new Jugadores();
+        JugadoresMadrid.setNombre("Ramos");
+
+        em.getTransaction().begin();
+        em.persist(JugadoresBarcelona);
+        em.persist(JugadoresMadrid);
+        em.getTransaction().commit();
+        
+        // Cerrar la conexión con la base de datos
+        em.close(); 
+        emf.close(); 
+        try { 
+            DriverManager.getConnection("jdbc:derby:AgendaJugadoresFutbolPU;shutdown=true"); 
+        } catch (SQLException ex) { 
+        }
     }
     
 }
